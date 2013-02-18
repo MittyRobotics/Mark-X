c9095bd4028fab1e24aaa6fdf2a9c4157fdd04b4
Vadim Korolik <vkorolik@gmail.com>
2013-02-18 15:27:40 -0800

    Camera PWM

--- MarkX_OfficialCode/MarkX.cpp
+++ MarkX_OfficialCode/MarkX.cpp
@@ -10,7 +10,6 @@
  * 1 single solenoid (spike), 2 double solenoids (spikes),
  * 4 double solenoids (each solenoid 2 ports on breakout
  * 
- * CHECK TKOCLIMBER LINE 68
  * FIX JAG PORTS/WINCH PORTS
  */
 
@@ -155,8 +154,11 @@ void MarkX::Operator()
 	if (stick3.GetRawButton(3))
 		rsFrontLoaderLift.SetOn(-1);
 	
-	if (stick3.GetRawButton(9))
+	if (stick3.GetRawButton(9) and stick3.GetX() > 0)
 		cameraServo.SetRaw(cameraServo.GetRaw() + CAMERA_PWM_INCREMENT);
+	
+	if (stick3.GetRawButton(9) and stick3.GetX() < 0)
+		cameraServo.SetRaw(cameraServo.GetRaw() - CAMERA_PWM_INCREMENT);
 	
 	if (stick1.GetRawButton(10))
 		JukeR();
